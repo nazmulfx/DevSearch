c86b8f327eabced37ad0f640694fb1db9f2e5925
nazmulfx <nazmulhossain664@gmail.com>
2024-01-28 13:11:47 +0600

    templates inheritance

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path('projects/', views.projects, name="projects"),
+    path('project/<str:pk>/', views.Singleproject, name='project')
 ]
--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Home Page')
+def projects(request):
+    
+    return render(request, 'projects.html')
+
+def Singleproject(request, pk):
+    
+    return render(request, 'single-project.html')
