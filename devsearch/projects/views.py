e53afc60c6ae59667c7574d897a835592500cfc4
nazmulfx <nazmulhossain664@gmail.com>
2024-02-25 12:11:09 +0600

    Pagination step-3

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -26,7 +26,7 @@ def projects(request):
         page = paginator.num_pages
         projects = paginator.page(page)
     
-    context = {'projects':projects, 'search_query':search_query}
+    context = {'projects':projects, 'search_query':search_query, 'paginator':paginator}
     return render(request, 'projects/projects.html', context)
 
 def Singleproject(request, pk):
